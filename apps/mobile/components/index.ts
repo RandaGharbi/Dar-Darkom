5a3ad55647747a95220e8cb523faa2dd12d45af4
Rinda Gharbi <randaa.gharbi@gmail.com>
2025-08-15 03:42:09 +0200

    fix: corrige l'export du composant Banner - résout l'erreur TypeScript

--- apps/mobile/components/index.ts
+++ apps/mobile/components/index.ts
@@ -1,6 +1,6 @@
 export { OrderCard } from './OrderCard';
 export { StatusBadge } from './StatusBadge';
-export { default as Banner } from './Banner';
+export { Banner } from './Banner';
 export { default as CartItemRow } from './CartItemRow';
 export { default as ContactInfo } from './ContactInfo';
 export { default as FavoriteButton } from './FavoriteButton';
